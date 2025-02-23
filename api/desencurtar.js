b851f8f8ed06d9afd8cf7cfd91e4fd8edd38341a
Sonhador Brasil <magazinegravatai@gmail.com>
2025-02-23 02:13:24 -0300

    Update desencurtar.js

--- api/desencurtar.js
+++ api/desencurtar.js
@@ -6,12 +6,21 @@ export default async function handler(req, res) {
     }
 
     try {
+        // Faz a requisição para obter a URL real (seguindo o redirecionamento)
         const response = await fetch(url, {
             method: 'GET',
-            redirect: 'follow',
+            redirect: 'follow', // Segue o redirecionamento da URL
         });
 
-        return res.status(200).json({ url: response.url });
+        // Verifica se a resposta foi bem-sucedida
+        if (response.ok) {
+            const finalUrl = response.url;
+            
+            // Exemplo de URL: https://www.tiktok.com/@bora.monetizar20/video/7460135168678972678?_r=1&_t=ZM-8u8zA8LCkM9
+            return res.status(200).json({ url: finalUrl });
+        } else {
+            return res.status(500).json({ error: "Erro ao acessar a URL." });
+        }
     } catch (error) {
         console.error(error);
         return res.status(500).json({ error: "Erro ao seguir o redirecionamento." });
